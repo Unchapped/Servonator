cee0092edc0ff919f8c34227ef04ee020c6ffcf6
Nathaniel Chapman <nate@nathanielchapman.com>
2025-12-14 21:01:55 -0800

    started hacky inlining example

---
+++ Arduino/adafruit_servo_inline/adafruit_servo_inline.ino
@@ -0,0 +1,85 @@
+#include <Wire.h>
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+// Depending on your servo make, the pulse width min and max may vary, you 
+// want these to be as small/large as possible without hitting the hard stop
+// for max range. You'll have to tweak them as necessary to match the servos you
+// have!
+#define SERVOMIN  120 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  460 // This is the 'maximum' pulse length count (out of 4096)
+#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+
+
+// Default PCA9685 I2C Slave Address
+#define PCA9685_I2C_ADDRESS 0x40
+#define PCA9685_OSC 25000000
+
+/******************* Low level I2C interface */
+uint8_t _read8(uint8_t addr) {
+  Wire.beginTransmission(PCA9685_I2C_ADDRESS);
+  Wire.write(addr);
+  Wire.endTransmission();
+
+  Wire.requestFrom(PCA9685_I2C_ADDRESS, (uint8_t)1);
+  return Wire.read();
+}
+
+void _write8(uint8_t addr, uint8_t d) {
+  Wire.beginTransmission(PCA9685_I2C_ADDRESS);
+  Wire.write(addr);
+  Wire.write(d);
+  Wire.endTransmission();
+}
+
+void inline _beginPWM() {
+  Wire.begin();
+  //void reset();
+  {
+    _write8(PCA9685_MODE1, MODE1_RESTART);
+    delay(10);
+  }
+
+  //pretty sure these were swapped unnecessarily in the Adafruit library
+  pwm.setOscillatorFrequency(PCA9685_OSC);
+  pwm.setPWMFreq(SERVO_FREQ);
+}
+
+uint8_t inline _setPWM(uint8_t num, uint16_t on, uint16_t off) {
+  Wire.beginTransmission(PCA9685_I2C_ADDRESS);
+  Wire.write(PCA9685_LED0_ON_L + 4 * num);
+  Wire.write(on);
+  Wire.write(on >> 8);
+  Wire.write(off);
+  Wire.write(off >> 8);
+  return Wire.endTransmission();
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("8 channel Servo test!");
+
+  _beginPWM();
+  //pwm.begin();
+  //pwm.setOscillatorFrequency(25000000); //THIS IS WHERE THE FUCKING BUG CAME FROM!
+  //pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+
+  delay(10);
+}
+
+
+
+uint16_t pulselen = SERVOMIN;
+int16_t dir = 1;
+
+void loop() {
+  if (pulselen == SERVOMIN) dir = 1;
+  if (pulselen == SERVOMAX) dir = -1;
+  pulselen += dir;
+
+  for (uint8_t servonum = 0; servonum < 16; servonum++) {
+    _setPWM(servonum, 0, pulselen);
+  }
+}
