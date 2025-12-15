a98a3a78103ae310641cafca2e1b36a21e47b9cf
Nathaniel Chapman <nate@nathanielchapman.com>
2025-12-14 22:41:23 -0800

    is totally borked, so I may live with the wire lib

--- Arduino/adafruit_servo_inline/adafruit_servo_inline.ino
+++ Arduino/adafruit_servo_inline/adafruit_servo_inline.ino
@@ -1,4 +1,106 @@
-#include <Wire.h>
+//#include <Wire.h>
+#include <Arduino.h>
+
+// Copied form i2cmaster.c because Arduino IDE kinda sux at path configuration
+/*************************************************************************
+* Title:    I2C master library using hardware TWI interface
+* Author:   Peter Fleury <pfleury@gmx.ch>  http://jump.to/fleury
+* File:     $Id: twimaster.c,v 1.4 2015/01/17 12:16:05 peter Exp $
+* Software: AVR-GCC 3.4.3 / avr-libc 1.2.3
+* Target:   any AVR device with hardware TWI 
+* Usage:    API compatible with I2C Software Library i2cmaster.h
+**************************************************************************/
+extern "C" {
+#include <avr/io.h>
+#include <compat/twi.h>
+}
+
+#define SCL_CLOCK  100000L
+/*************************************************************************
+ Initialization of the I2C bus interface. Need to be called only once
+*************************************************************************/
+void i2c_init(void)
+{
+  /* initialize TWI clock: 100 kHz clock, TWPS = 0 => prescaler = 1 */
+  
+  TWSR = 0;                         /* no prescaler */
+  TWBR = ((F_CPU/SCL_CLOCK)-16)/2;  /* must be > 10 for stable operation */
+
+}/* i2c_init */
+
+
+/*************************************************************************	
+  Issues a start condition and sends address and transfer direction.
+  return 0 = device accessible, 1= failed to access device
+*************************************************************************/
+unsigned char i2c_start(unsigned char address)
+{
+    uint8_t   twst;
+
+	// send START condition
+	TWCR = (1<<TWINT) | (1<<TWSTA) | (1<<TWEN);
+
+	// wait until transmission completed
+	while(!(TWCR & (1<<TWINT)));
+
+	// check value of TWI Status Register. Mask prescaler bits.
+	twst = TW_STATUS & 0xF8;
+	if ( (twst != TW_START) && (twst != TW_REP_START)) return 1;
+
+	// send device address
+	TWDR = address;
+	TWCR = (1<<TWINT) | (1<<TWEN);
+
+	// wail until transmission completed and ACK/NACK has been received
+	while(!(TWCR & (1<<TWINT)));
+
+	// check value of TWI Status Register. Mask prescaler bits.
+	twst = TW_STATUS & 0xF8;
+	if ( (twst != TW_MT_SLA_ACK) && (twst != TW_MR_SLA_ACK) ) return 1;
+
+	return 0;
+
+}/* i2c_start */
+
+/*************************************************************************
+  Send one byte to I2C device
+  
+  Input:    byte to be transfered
+  Return:   0 write successful 
+            1 write failed
+*************************************************************************/
+unsigned char i2c_write( unsigned char data )
+{	
+    uint8_t   twst;
+    
+	// send data to the previously addressed device
+	TWDR = data;
+	TWCR = (1<<TWINT) | (1<<TWEN);
+
+	// wait until transmission completed
+	while(!(TWCR & (1<<TWINT)));
+
+	// check value of TWI Status Register. Mask prescaler bits
+	twst = TW_STATUS & 0xF8;
+	if( twst != TW_MT_DATA_ACK) return 1;
+	return 0;
+
+}/* i2c_write */
+
+/*************************************************************************
+ Terminates the data transfer and releases the I2C bus
+*************************************************************************/
+void i2c_stop(void)
+{
+    /* send stop condition */
+	TWCR = (1<<TWINT) | (1<<TWEN) | (1<<TWSTO);
+	
+	// wait until stop condition is executed and bus released
+	while(TWCR & (1<<TWSTO));
+
+}/* i2c_stop */
+
+
 
 //Necessary PWM Defines
 #define PCA9685_I2C_ADDRESS 0x40
@@ -17,32 +119,42 @@
 #define SERVOMIN  120 // This is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  460 // This is the 'maximum' pulse length count (out of 4096)
 
+
 void _write8(uint8_t addr, uint8_t d) {
-  Wire.beginTransmission(PCA9685_I2C_ADDRESS);
-  Wire.write(addr);
-  Wire.write(d);
-  Wire.endTransmission();
+  i2c_start(PCA9685_I2C_ADDRESS);
+  i2c_write(addr);
+  i2c_write(d);
+  i2c_stop();
 }
 
+// TODO: consider porting this to pfleury's twimaster library
+// uint8_t _write8(uint8_t addr, uint8_t d) {
+//   uint8_t twi_buf[] = {addr,d};
+//   return twi_writeTo(PCA9685_I2C_ADDRESS, twi_buf, 2, 1, true);
+// }
+
 void inline _beginPWM() {
-  Wire.begin();
+  i2c_init(); //Wire.begin();
   // _write8(PCA9685_MODE1, MODE1_SLEEP); // go to sleep
   _write8(PCA9685_PRESCALE, SERVO_PRESCALE); // set the prescaler
   _write8(PCA9685_MODE1, MODE1_RESTART | MODE1_AI); // Wake up
 }
 
-uint8_t inline _setPWM(uint8_t num, uint16_t on, uint16_t off) {
-  Wire.beginTransmission(PCA9685_I2C_ADDRESS);
-  Wire.write(PCA9685_LED0_ON_L + 4 * num);
-  Wire.write(on);
-  Wire.write(on >> 8);
-  Wire.write(off);
-  Wire.write(off >> 8);
-  return Wire.endTransmission();
+//TODO: since we have a local buffer, we should just do all 16 channels in one transaction
+void inline _setPWM(uint8_t num, uint16_t on, uint16_t off) {
+  i2c_start(PCA9685_I2C_ADDRESS);
+  i2c_write(PCA9685_LED0_ON_L + 4 * num);
+  i2c_write(on);
+  i2c_write(on >> 8);
+  i2c_write(off);
+  i2c_write(off >> 8);
+  i2c_stop();
+  //return Wire.endTransmission();
 }
 
 
 void setup() {
+  Serial.begin(9600);
   _beginPWM();
 }
 
