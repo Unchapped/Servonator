6454c897dd3ee16adebf502ccb767994d7d8c27b
Nathaniel Chapman <nate@nathanielchapman.com>
2025-12-14 16:07:19 -0800

    Kinda terrifying that this works...
    lots of further trimming down to be done here though

--- Arduino/Servonator_Inline_Nano/Servonator_Inline_Nano.ino
+++ Arduino/Servonator_Inline_Nano/Servonator_Inline_Nano.ino
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
+// #include <Adafruit_PWMServoDriver.h>
 
 //Nano Wiring details
 //Pins D0-D1 - (RS-485 RX/TX lines)
@@ -30,12 +30,6 @@ inline void setup_offset_pins() {
 //A6-A7 - Potentiometer Axes 5-6
 #define NUM_POTS 6
 const uint8_t POT_PINS[NUM_POTS] = {A0,A1,A2,A3,A6,A7};
-
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-#define SERVOMIN  120 // This is the 'minimum' pulse length count (out of 4096)
-#define SERVOMAX  460 // This is the 'maximum' pulse length count (out of 4096)
-#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
-
 
 // Array of DMX values (Only 16 channels, adjusted by offset)
 uint8_t dmxData[16];
@@ -128,15 +122,135 @@ ISR(USART_RX_vect)
   }
 }
 
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+
+/*!
+ *  lots of these #defines copied from the Adafruit_PWMServoDriver library
+ *
+ *  This is a library for our Adafruit 16-channel PWM & Servo driver.
+ *
+ *  Designed specifically to work with the Adafruit 16-channel PWM & Servo
+ * driver.
+ *
+ *  Pick one up today in the adafruit shop!
+ *  ------> https://www.adafruit.com/product/815
+ *
+ *  These driver use I2C to communicate, 2 pins are required to interface.
+ *  For Arduino UNOs, thats SCL -> Analog 5, SDA -> Analog 4.
+ *
+ *  Adafruit invests time and resources providing this open source code,
+ *  please support Adafruit andopen-source hardware by purchasing products
+ *  from Adafruit!
+ *
+ *  Limor Fried/Ladyada (Adafruit Industries).
+ *
+ *  BSD license, all text above must be included in any redistribution
+ */
+
+//TODO: These may need rejiggered with the different oscillator frequency
+#define SERVOMIN  120 // This is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX  460 // This is the 'maximum' pulse length count (out of 4096)
+
+
+/******************* Low level I2C interface */
+// #define _i2c Wire
+// #define _i2caddr PCA9685_I2C_ADDRESS
+
+// Default PCA9685 I2C Slave Address
+#define PCA9685_I2C_ADDRESS 0x40
+
+
+#define PCA9685_MODE1 0x00      /**< Mode Register 1 */
+// #define MODE1_ALLCAL 0x01  /**< respond to LED All Call I2C-bus address */
+// #define MODE1_SUB3 0x02    /**< respond to I2C-bus subaddress 3 */
+// #define MODE1_SUB2 0x04    /**< respond to I2C-bus subaddress 2 */
+// #define MODE1_SUB1 0x08    /**< respond to I2C-bus subaddress 1 */
+#define MODE1_SLEEP 0x10   /**< Low power mode. Oscillator off */
+#define MODE1_AI 0x20      /**< Auto-Increment enabled */
+// #define MODE1_EXTCLK 0x40  /**< Use EXTCLK pin clock */
+#define MODE1_RESTART 0x80 /**< Restart enabled */
+
+// #define PCA9685_MODE2 0x01      /**< Mode Register 2 */
+// #define MODE2_OUTNE_0 0x01 /**< Active LOW output enable input */
+// #define MODE2_OUTNE_1 0x02 /**< Active LOW output enable input - high impedience */
+// #define MODE2_OUTDRV 0x04 /**< totem pole structure vs open-drain */
+// #define MODE2_OCH 0x08    /**< Outputs change on ACK vs STOP */
+// #define MODE2_INVRT 0x10  /**< Output logic state inverted */
+
+#define PCA9685_LED0_ON_L 0x06  /**< LED0 on tick, low byte*/
+// #define PCA9685_LED0_ON_H 0x07  /**< LED0 on tick, high byte*/
+// #define PCA9685_LED0_OFF_L 0x08 /**< LED0 off tick, low byte */
+// #define PCA9685_LED0_OFF_H 0x09 /**< LED0 off tick, high byte */
+
+#define PCA9685_PRESCALE 0xFE     /**< Prescaler for PWM output frequency */
+
+// 25 MHz Internal PCA9685 Osclillator Frequency
+//#define PCA9685_OSC 25000000
+//#define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
+// uint8_t prescale = ((PCA9685_OSC / (SERVO_FREQ * 4096.0)) + 0.5) - 1; = 121
+#define SERVO_PRESCALE 121
+
+uint8_t pwm_read8(uint8_t addr) {
+  Wire.beginTransmission(PCA9685_I2C_ADDRESS);
+  Wire.write(addr);
+  Wire.endTransmission();
+  Wire.requestFrom((uint8_t)PCA9685_I2C_ADDRESS, (uint8_t)1);
+  return Wire.read();
+}
+
+void pwm_write8(uint8_t addr, uint8_t d) {
+  Wire.beginTransmission(PCA9685_I2C_ADDRESS);
+  Wire.write(addr);
+  Wire.write(d);
+  Wire.endTransmission();
+}
+
+uint8_t pwm_setPWM(uint8_t num, uint16_t on, uint16_t off) {
+  Wire.beginTransmission(PCA9685_I2C_ADDRESS);
+  Wire.write(PCA9685_LED0_ON_L + 4 * num);
+  Wire.write(on);
+  Wire.write(on >> 8);
+  Wire.write(off);
+  Wire.write(off >> 8);
+  return Wire.endTransmission();
+}
+
+inline void setup_pwm() {
+  //Initialize Adafruit PWM
+  //pwm.begin();
+  {
+    Wire.begin();
+
+    //MODE1 Default Value starts up in sleep mode, and will respond to all call addresses
+    //reset(); //NATE: Assuming we're starting from power-off, I don't think we need this...
+    {
+      pwm_write8(PCA9685_MODE1, MODE1_RESTART);
+      delay(10); //wait for restart (should only be 500 us)
+    }
+    //pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+    {
+        uint8_t oldmode = pwm_read8(PCA9685_MODE1); //Don't know this is needed
+        // uint8_t newmode = (oldmode & ~MODE1_RESTART) | MODE1_SLEEP; // sleep //this should be the default state
+        // write8(PCA9685_MODE1, newmode);                             // go to sleep
+        pwm_write8(PCA9685_PRESCALE, SERVO_PRESCALE); // set the prescaler
+        pwm_write8(PCA9685_MODE1, oldmode);
+        delay(5);
+        // This sets the MODE1 register to turn on auto increment.
+        pwm_write8(PCA9685_MODE1, oldmode | MODE1_RESTART | MODE1_AI);
+    }
+
+
+  //}
+  }
+}
+
+//End Adafruit lib copy/refines
 
 void setup() {
   setup_dmx();
   setup_offset_pins();
-
-  //Initialize Adafruit PWM
-  pwm.begin();
-  pwm.setOscillatorFrequency(27000000);
-  pwm.setPWMFreq(SERVO_FREQ);  // Analog servos run at ~50 Hz updates
+  setup_pwm();
+  
 
   //Enable Power LED
   pinMode(PWR_LED, OUTPUT);
@@ -153,7 +267,7 @@ void dmx_loop() {
   if(DMX_STATE_REG & DMX_UPDATE_FLAG) {
     digitalWrite(DMX_LED, HIGH);
     for(int channel = 0; channel < 16; channel++) {
-        pwm.setPWM(channel, 0, map(dmxData[channel], 0, 255, SERVOMIN, SERVOMAX));
+        pwm_setPWM(channel, 0, map(dmxData[channel], 0, 255, SERVOMIN, SERVOMAX));
       }
     dmx_last_packet = millis();
     DMX_STATE_REG &= ~DMX_UPDATE_FLAG; //clear DMX Update Flag
@@ -166,7 +280,7 @@ void dmx_loop() {
 void manual_loop() {
   digitalWrite(DMX_LED, LOW);
   for(int channel = 0; channel < NUM_POTS; channel++)
-    pwm.setPWM(channel, 0, map(analogRead(POT_PINS[channel]), 0, 1023, SERVOMIN, SERVOMAX));
+    pwm_setPWM(channel, 0, map(analogRead(POT_PINS[channel]), 0, 1023, SERVOMIN, SERVOMAX));
 }
 
 void loop() {
